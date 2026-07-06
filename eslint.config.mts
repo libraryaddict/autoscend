147f0960bb6ccaf621bdd754621757b6f3c568d9
libraryaddict <libraryaddict@users.noreply.github.com>
2026-07-06 20:46:34 +1200

    Basic setup of eslint, prettier

---
+++ eslint.config.mts
@@ -0,0 +1,47 @@
+import * as eslint from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettier from "eslint-config-prettier";
+import libram, { verifyConstantsSinceRevision } from "eslint-plugin-libram";
+import tseslint from "typescript-eslint";
+
+const KOLMAFIA_VERSION = 28984;
+await verifyConstantsSinceRevision(KOLMAFIA_VERSION);
+
+export default defineConfig(
+  globalIgnores(["dist/**"]),
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...libram.configs.recommended,
+  {
+    files: ["src/**/*.ts", "src/**/*.tsx", "**/*.ts", "**/*.tsx"],
+    rules: {
+      "block-scoped-var": "error",
+      "eol-last": "error",
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "no-var": "error",
+      "prefer-arrow-callback": "error",
+      "prefer-const": "error",
+      "prefer-template": "error",
+      "no-unused-vars": "error",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector: "TSEnumDeclaration:not([const=true])",
+          message: "Don't declare non-const enums",
+        },
+      ],
+      "sort-imports": [
+        "error",
+        {
+          ignoreCase: true,
+          ignoreDeclarationSort: true,
+        },
+      ],
+      "libram/verify-constants": "error",
+    },
+  },
+  prettier,
+);
--- src/c2t_apron.ts
+++ src/c2t_apron.ts
@@ -1,4 +1,20 @@
-import { Item, Stat, abort, cliExecute, containsText, fullnessLimit, getProperty, itemAmount, myFullness, myHash, myPrimestat, print, splitString, toLowerCase, visitUrl } from "kolmafia";
+import {
+  Item,
+  Stat,
+  abort,
+  cliExecute,
+  containsText,
+  fullnessLimit,
+  getProperty,
+  itemAmount,
+  myFullness,
+  myHash,
+  myPrimestat,
+  print,
+  splitString,
+  toLowerCase,
+  visitUrl,
+} from "kolmafia";
 import { AshMatcher } from "./autoscend/utils/kolmafiaUtils";
 
 //c2t apron
@@ -6,106 +22,101 @@ import { AshMatcher } from "./autoscend/utils/kolmafiaUtils";
 //deals with the black and white apron meal kit
 //simply selects meal based on stat and selects all the extra ingredients that are available
 //cli flag
-let c2t_apron_cli: boolean = false;
+const c2t_apron_cli: boolean = false;
 //eats a meal kit with a provided stat
-export function c2t_apron$1(): boolean { return c2t_apron(myPrimestat()); }
-//CLI handling
-function main$c2t_apron(...stat_select: string[]): void {
-	c2t_apron_cli = true;
-
-	if (stat_select.length === 0)
-		{ c2t_apron$1(); }
-	else { switch (toLowerCase((stat_select[0] ??= ""))) {
-		default:
-			c2t_apron_error(`"${(stat_select[0] ??= "")}" is not a valid stat`);
-			break;
-		case "mus":
-		case "musc":
-		case "muscle":
-			c2t_apron(Stat.get("Muscle"));
-			break;
-		case "mys":
-		case "myst":
-		case "mysticality":
-			c2t_apron(Stat.get("Mysticality"));
-			break;
-		case "mox":
-		case "moxie":
-			c2t_apron(Stat.get("Moxie"));
-			break;
-	} }
+export function c2t_apron$1(): boolean {
+  return c2t_apron(myPrimestat());
 }
+//CLI handling
 
 //returns true on success
 function c2t_apron(select: Stat): boolean {
-	let meal: number = 0;
-	let kit: Item = Item.get("Black and White Apron Meal Kit");
-	let page: string;
-	let mat: AshMatcher;
-	let sendit: string = "";
-	let start_1: number = myFullness();
-	let startKits: number = itemAmount(kit);
-	let allowlist: Map<string, boolean> = new Map();
+  let meal: number = 0;
+  const kit: Item = Item.get("Black and White Apron Meal Kit");
+  let page: string;
+  let mat: AshMatcher;
+  let sendit: string = "";
+  const start_1: number = myFullness();
+  const startKits: number = itemAmount(kit);
+  let allowlist: Map<string, boolean> = new Map();
 
-	switch (select) {
-		default:
-			return c2t_apron_error(`"${select}" is not a valid stat`);
-		case Stat.get("Muscle"):
-			meal = 0;
-			break;
-		case Stat.get("Mysticality"):
-			meal = 1;
-			break;
-		case Stat.get("Moxie"):
-			meal = 2;
-			break;
-	}
-	if (itemAmount(kit) === 0)
-		{ return c2t_apron_error(`no ${kit} on hand`); }
-	if (myFullness() + 3 > fullnessLimit())
-		{ return c2t_apron_error(`too full to eat a ${kit}`); }
+  switch (select) {
+    default:
+      return c2t_apron_error(`"${select}" is not a valid stat`);
+    case Stat.get("Muscle"):
+      meal = 0;
+      break;
+    case Stat.get("Mysticality"):
+      meal = 1;
+      break;
+    case Stat.get("Moxie"):
+      meal = 2;
+      break;
+  }
+  if (itemAmount(kit) === 0) {
+    return c2t_apron_error(`no ${kit} on hand`);
+  }
+  if (myFullness() + 3 > fullnessLimit()) {
+    return c2t_apron_error(`too full to eat a ${kit}`);
+  }
 
-	page = visitUrl(`inv_use.php?pwd=${myHash()}&which=3&whichitem=${kit.id}`, false, true);
-	mat = new AshMatcher(`name="ingredients${meal}\\[\\]"\\s+value="(\\d+)"\\s+data-has="(\\d)"`, page);
-	allowlist = c2t_apron_allowlist();
+  page = visitUrl(
+    `inv_use.php?pwd=${myHash()}&which=3&whichitem=${kit.id}`,
+    false,
+    true,
+  );
+  mat = new AshMatcher(
+    `name="ingredients${meal}\\[\\]"\\s+value="(\\d+)"\\s+data-has="(\\d)"`,
+    page,
+  );
+  allowlist = c2t_apron_allowlist();
 
-	sendit = `choice.php?pwd&whichchoice=1518&option=1&meal=${meal}`;
-	while (mat.find())
-		{ if (mat.group(2) === "1" && allowlist.has(mat.group(1)))
-			{ sendit += `&ingredients${meal}[]=${mat.group(1)}`; } }
+  sendit = `choice.php?pwd&whichchoice=1518&option=1&meal=${meal}`;
+  while (mat.find()) {
+    if (mat.group(2) === "1" && allowlist.has(mat.group(1))) {
+      sendit += `&ingredients${meal}[]=${mat.group(1)}`;
+    }
+  }
 
-	page = visitUrl(sendit, true, false);
+  page = visitUrl(sendit, true, false);
 
-	cliExecute("refresh inv");
+  cliExecute("refresh inv");
 
-	if (start_1 < myFullness() || startKits > itemAmount(kit) || containsText(page, "<br>You cook and quickly consume your"))
-	{
-		return true;
-	}
-	return c2t_apron_error(`did not eat the ${kit}`);
+  if (
+    start_1 < myFullness() ||
+    startKits > itemAmount(kit) ||
+    containsText(page, "<br>You cook and quickly consume your")
+  ) {
+    return true;
+  }
+  return c2t_apron_error(`did not eat the ${kit}`);
 }
 
 //map of ingredients on the allowlist
 function c2t_apron_allowlist(): Map<string, boolean> {
-	let out: Map<string, boolean> = new Map();
-	let split: Map<number, string> = new Map();
+  const out: Map<string, boolean> = new Map();
+  let split: Map<number, string> = new Map();
 
-	if (getProperty("c2t_apron_allowlist") !== "")
-		{ split = new Map(splitString(getProperty("c2t_apron_allowlist"), ",").map((_v, _i) => [_i, _v])); }
+  if (getProperty("c2t_apron_allowlist") !== "") {
+    split = new Map(
+      splitString(getProperty("c2t_apron_allowlist"), ",").map((_v, _i) => [
+        _i,
+        _v,
+      ]),
+    );
+  }
 
-	for (let [i, x] of split)
-		{ out.set(x, true); }
+  for (const [i, x] of split) {
+    out.set(x, true);
+  }
 
-	return out;
+  return out;
 }
 
 //errors
 function c2t_apron_error(msg: string): boolean {
-	let out: string = `c2t_apron error: ${msg}`;
+  const out: string = `c2t_apron error: ${msg}`;
 
-	if (c2t_apron_cli)
-		{ abort(out); }
-
-	print(out, "red");
-	return false;
+  print(out, "red");
+  return false;
 }
