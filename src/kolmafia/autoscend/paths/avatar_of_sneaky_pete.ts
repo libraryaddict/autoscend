e1e20ac254f4d6632a22828c17e08a5a2d1bac16
libraryaddict <libraryaddict@users.noreply.github.com>
2026-07-23 17:16:31 +1200

    Fix a leftover import

--- src/kolmafia/autoscend/paths/avatar_of_sneaky_pete.ts
+++ src/kolmafia/autoscend/paths/avatar_of_sneaky_pete.ts
@@ -16,7 +16,7 @@ import { $item, $path, $skill } from "libram";
 
 import { acquireHermitItem, pullXWhenHaveY } from "../auto_acquire";
 import { equipBaseline } from "../auto_equipment";
-import { auto_log_info, auto_log_info, ovenHandle } from "../auto_util";
+import { auto_log_info, ovenHandle } from "../auto_util";
 import { AshMatcher } from "../utils/kolmafiaUtils";
 
 //Defined in autoscend/paths/avatar_of_sneaky_pete.ash
